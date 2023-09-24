ba23eec18f6bd5353bb0198cb3a8714982cb3393
Alesia_Shikurova <ales2503@yandex.by>
2023-09-24 14:31:19 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+//Кнопка переход со ссылкой 
+document.querySelector('.abonement_btn').onclick = function(e) {
+    window.open('_blank').location = 'https://go.yandex/ru_ru/lp/rides/scooter';
+};
+document.querySelector('.header_btn').onclick = function(e) {
+    window.open('_blank').location = 'https://go.yandex/ru_ru/lp/rides/scooter';
+};
+
+
+//Слайдер
+  /* Устанавливаем стартовый индекс слайда по умолчанию: */
+let slideIndex = 1;
+/* Вызываем функцию, которая реализована ниже: */
+showSlides(slideIndex);
+
+/* Увеличиваем индекс на 1 — показываем следующий слайд: */
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
+function previousSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+/* Устанавливаем текущий слайд: */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Функция перелистывания: */
+function showSlides(n) {
+    /* Обращаемся к элементам с названием класса "item", то есть к картинкам: */
+    let slides = document.getElementsByClassName("item");
+    
+    /* Проверяем количество слайдов: */
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    /* Проходим по каждому слайду в цикле for: */
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    /* Делаем элемент блочным: */
+    slides[slideIndex - 1].style.display = "block";    
+}
